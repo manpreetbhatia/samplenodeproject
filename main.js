8c6ebfb938972f44858a1fde9981c4605cea4ade
manpreetbhatia <bhatiamanpreet2003@gmail.com>
2016-07-12 10:30:45 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("testtting node js");
+var http=require("http");
+function handler(request,response) {
+    response.writeHead(200,{'Content-type':'text/html'});
+    response.end('<html>First Name: <input type="text" placeholder="Enter first name!" name="fname" value=""></html>');
+    response.end('testing node js\n');
+}
+var server=http.createServer(handler);
+server.listen(8081);
+console.log('Server running at http://127.0.0.1:8081/');
